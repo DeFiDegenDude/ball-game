9f2a204a2dbf4543d9594677e7a04232a7c43752
DFDdude <adamrogersyt@gmail.com>
2025-01-17 19:03:57 -0500

    Add files via upload

---
+++ randomusernamehtml.py
@@ -0,0 +1,3 @@
+import randomusername
+while i == 1:
+    print(i)
---
+++ weapchooser.py
@@ -0,0 +1,4 @@
+weap = input('Choose your weapon:\n\n1.M4A4\n2.Barrett M82\n3.M16A1\n4.MG42\n5.M1 Garand\n\n')
+if weap == "M4A4" or weap == "1" or weap == "1.":
+    print("You're weapon is M4A4")
+else: print("Gotcha")
